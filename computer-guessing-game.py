6cf66fb9cc14d15fb00851eb21524367620fae5b
jay20162016 <jayjay.jay20162016@gmail.com>
2016-07-06 09:30:48 -0400

    Update computer-guessing-game.py

--- computer-guessing-game.py
+++ computer-guessing-game.py
@@ -1,3 +1,27 @@
 #!/usr/bin/python
 
+#this is the game where you pick a int and the computer guesses it.
 
+num = int(raw_input('what integer from 0 to 1,000 do you pick for the computer to guess??'))
+guess = 1000
+error='high'
+score = 0
+while True:
+  if error == 'high':
+    guess = guess / 2
+  else:
+    test = guess / 2
+    guess = guess - test
+    del test
+  print 'guess: ',guess
+  if guess == num :
+    print "computer's score :",score
+    break
+  else:
+    score += 1
+    if guess < num:
+      print 'too high'
+      error = 'high'
+    else:
+      print 'too low'
+      error = 'low
